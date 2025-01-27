c34ffe07abba3aeaebb12d1be632fd06aa62e7a9
Renzo <jcapazape2020@gmail.com>
2025-01-26 20:32:48 -0500

    Update settings.py

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -174,5 +174,6 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://django-02-production.up.railway.app',  # Reemplaza esto con tu URL de producción en Railway
+    "https://django-02-production.up.railway.app",
+    "https://grafitacna-08-production.up.railway.app", # Reemplaza esto con tu URL de producción en Railway
 ]
