c1f915ab9976545d7e4fde05b97b2800dee98855
Renzo <jcapazape2020@gmail.com>
2025-07-15 22:49:17 -0500

    Update settings.py

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -187,12 +187,12 @@ CORS_ALLOWED_ORIGINS = [
     "https://django-02-production.up.railway.app",
     "https://grafitacna-07-production.up.railway.app",
     "https://grafitacna-08-production.up.railway.app",
-    "https://grafitacna.netlify.app/",
+    "https://grafitacna.netlify.app",
 
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://django-02-production.up.railway.app",
     "https://grafitacna-08-production.up.railway.app", # Reemplaza esto con tu URL de producción en Railway
-    "https://grafitacna.netlify.app/",
+    "https://grafitacna.netlify.app",
 ]
