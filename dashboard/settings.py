6e73284b4cd330342b9b4c5b31528d2b933d4e83
Renzo <jcapazape2020@gmail.com>
2025-07-15 23:36:44 -0500

    Update settings.py

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -184,6 +184,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # Cambia según el puerto usado por Vite
     "https://grafitacnaperu.vercel.app",
     "http://localhost:4173",
+    "http://localhost:3000",
     "https://django-02-production.up.railway.app",
     "https://grafitacna-07-production.up.railway.app",
     "https://grafitacna-08-production.up.railway.app",
